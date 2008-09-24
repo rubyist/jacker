bd7d4bab5adaef1199032fcf73c29950842e0f4a
Scott Barron <scott@elitists.net>
2008-09-24 08:14:42 -0400

    Add rubycocoa app

---
+++ Jacker/JackerController.rb
@@ -0,0 +1,55 @@
+#
+#  JackerController.rb
+#  JackerApp
+#
+#  Created by Scott Barron on 9/23/08.
+#  Copyright (c) 2008. All rights reserved.
+#
+
+require 'osx/cocoa'
+
+require 'rubygems'
+require 'jacker'
+
+class JackerController < OSX::NSWindowController
+  include OSX
+  
+  ib_outlet :theMenu
+  ib_outlet :jackItem
+  
+  ib_action :jack_it
+  ib_action :startJacking
+  
+  def awakeFromNib
+    statusbar = NSStatusBar.systemStatusBar
+    item = statusbar.statusItemWithLength(NSVariableStatusItemLength)
+    item.setTitle "J"
+    
+    item.setMenu(@theMenu)
+    item.setEnabled(true)
+    setMenuStatus
+  end
+  
+  def jack_it(sender)
+    if Jacker.running?
+      self.stopJacking
+    else
+      showWindow(self)
+    end
+  end
+  
+  def startJacking(sender)
+    close
+    Jacker.start sender.stringValue
+    setMenuStatus
+  end
+  
+  def stopJacking
+    Jacker.stop
+    setMenuStatus
+  end
+  
+  def setMenuStatus
+    @jackItem.setTitle Jacker.running? ? "Stop Jacking" : "Start Jacking"
+  end
+end
---
+++ Jacker/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Jacker
+//
+//  Created by Scott Barron on 9/24/08.
+//  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
